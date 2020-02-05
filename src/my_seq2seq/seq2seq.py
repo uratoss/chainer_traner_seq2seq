aa1ebb1797bdce7ea57c633c95af7ea71f103a1f
uratoss <adlersschanze@gmail.com>
2020-02-06 08:51:16 +0900

    Squashed commit of the following:
    commit ec37911a8dcff343877998c39c2da860621bebf0
    Author: uratoss <adlersschanze@gmail.com>
    Date:   Thu Feb 6 08:48:55 2020 +0900
    
        change gitignore
    
    commit 277e982ff109b4624790239928544b121eb7b6ab
    Author: uratoss <adlersschanze@gmail.com>
    Date:   Thu Feb 6 08:47:06 2020 +0900
    
        use lib in generator
    
    commit 02fea06fc8084dcc70c9d1a34e65d2c30ee33c24
    Author: uratoss <adlersschanze@gmail.com>
    Date:   Thu Feb 6 08:40:56 2020 +0900
    
        split files
    
        make lib

---
+++ src/my_seq2seq/seq2seq.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import chainer
+
+import chainer.links as L
+import chainer.functions as F
+
+import numpy as xp
+
+# {{{ Seq2seq(chainer.Chain)
+# sequence to sequence class
+# This class output reply sequences if you input utterance data
+
+
+class Seq2seq(chainer.Chain):
+    def __init__(self, n_vocab, n_lay=1, n_unit=100, dropout=0.5):
+        super().__init__()
+        with self.init_scope():
+            self.embedx = L.EmbedID(n_vocab, n_unit)
+            self.embedy = L.EmbedID(n_vocab, n_unit)
+            self.encoder = L.NStepLSTM(
+                n_lay, n_unit, n_unit, dropout)
+            self.decoder = L.NStepLSTM(
+                n_lay, n_unit, n_unit, dropout)
+            self.W = L.Linear(n_unit, n_vocab)
+
+    def __call__(self, xs, ts=None, hx=None, cx=None, max_size=30):
+        # エンコーダ側の処理
+        xs_embeded = [self.embedx(x[:-1]) for x in xs]
+        hx, cx, _ = self.encoder(hx, cx, xs_embeded)
+        # デコーダ側の処理
+        if ts is None:
+            ys = [xp.array([x[-1]], dtype=xp.int32) for x in xs]
+            ys_list = []
+            for i in range(max_size):
+                ys_embeded = [self.embedy(y) for y in ys]
+                hx, cx, ys_embeded = self.decoder(hx, cx, ys_embeded)
+                ys = [xp.reshape(xp.argmax(F.softmax(self.W(y_embeded)).data), (1))
+                      for y_embeded in ys_embeded]
+                ys_list.append(ys)
+            ys = [xp.array([x[-1]], dtype=xp.int32) for x in xs]
+            ys_list.append(ys)
+            return ys_list
+        else:
+            ts = [F.concat((xp.reshape(t[-1],(1)), t[:-1]), axis=0) for t in ts]
+            ts_embeded = [self.embedy(t) for t in ts]
+            _, _, ys_embeded = self.decoder(hx, cx, ts_embeded)
+            ys = [self.W(y) for y in ys_embeded]
+            return ys
+# }}}
