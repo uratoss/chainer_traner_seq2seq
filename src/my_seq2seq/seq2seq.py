886f0cf13d89186a1d5785cd6f41a72c7196ad6c
uratoss <adlersschanze@gmail.com>
2020-02-10 22:50:29 +0900

    bug

--- src/my_seq2seq/seq2seq.py
+++ src/my_seq2seq/seq2seq.py
@@ -64,93 +64,26 @@ class GAtt(chainer.Chain):
             self.W1 = L.Linear(2 * n_unit, n_unit)
             self.W2 = L.Linear(2 * n_unit, n_unit)
             self.W3 = L.Linear(2 * n_unit, n_unit)
-            self.Wa1 = L.Linear(n_unit, n_unit)
-            self.Wa2 = L.Linear(n_unit, n_unit)
-            self.Wa3 = L.Linear(n_unit, n_unit)
             self.Wc1 = L.Linear(n_unit, n_unit)
             self.Wc2 = L.Linear(n_unit, n_unit)
-            self.Wc3 = L.Linear(n_unit, n_unit)
 
-    # 'dotScore' {{{
-    def scoreDot(self, attentions, ys_embeded_list):
-        ys = []
-        for attention, ys_embeded in zip(attentions, ys_embeded_list):
-            # attention -> 1文のエンコーダの出力
-            # ys_embeded -> 1文のデコーダの出力
-            attention = self.W3(attention)
-            target = []
-            for y_embeded in ys_embeded:
-                y_embeded = F.reshape(y_embeded, (1, y_embeded.shape[0]))
-                # y_embeded -> デコーダLSTMブロックの1単語の出力
-                # ctを計算する
-                s = 0.0
-                scores = []
-                for enc in attention:
-                    enc = F.reshape(enc, (1, enc.shape[0]))
-                    # ecn -> エンコーダLSTMブロックの1単語の出力
-                    scores.append(
-                        F.exp(F.matmul(y_embeded, enc, transb=True)).data[0][0])
-                    s += scores[-1]
-                ct = xp.zeros((1, y_embeded.shape[-1]), dtype=xp.float32)
-                for enc, score in zip(attention, scores):
-                    # ecn -> エンコーダLSTMブロックの1単語の出力
-                    # score -> 各エンコーダの出力に対するdot積の結果
-                    alpi = score / s
-                    ct += alpi * enc.data[0]
-                # ctから出力単語を作る
-                ct = chainer.Variable(ct)
-                target.append(F.tanh(self.Wc1(ct) + self.Wc2(y_embeded)))
-            ys.append(F.concat(target, axis=0))
-        return ys
-
-    # }}}
-
-    # 'generalScore' {{{
-    def scoreGeneral(self, attentions, ys_embeded_list):
-        ys = []
-        for attention, ys_embeded in zip(attentions, ys_embeded_list):
-            # attention -> 1文のエンコーダの出力
-            # ys_embeded -> 1文のデコーダの出力
-
-            # エンコーダの出力を順方向・逆方向に分ける
-            attention_f, attention_b = F.split_axis(attention, 2, axis=1)
-            target = []
-            for y_embeded in ys_embeded:
-                y_embeded = F.reshape(y_embeded, (1, y_embeded.shape[0]))
-                # y_embeded -> デコーダLSTMブロックの1単語の出力
-                # ctを計算する
-                s = 0.0
-                scores = []
-                for enc_f, enc_b in zip(attention_f, attention_b):
-                    enc_f = F.reshape(enc_f, (1, enc_f.shape[0]))
-                    enc_b = F.reshape(enc_b, (1, enc_b.shape[0]))
-                    # ecn_f -> 順方向エンコーダLSTMブロックの1単語の出力
-                    # ecn_b -> 逆方向エンコーダLSTMブロックの1単語の出力
-                    scores.append(
-                        F.exp(self.Wa1(enc_f) + self.Wa2(enc_b) + self.Wa3(y_embeded)).data[
-                            0
-                        ][0]
-                    )
-                    s += scores[-1]
-                ct_f = xp.zeros((1, y_embeded.shape[-1]), dtype=xp.float32)
-                ct_b = xp.zeros((1, y_embeded.shape[-1]), dtype=xp.float32)
-                for enc_f, enc_b, score in zip(attention_f, attention_b, scores):
-                    enc_f = F.reshape(enc_f, (1, enc_f.shape[0]))
-                    enc_b = F.reshape(enc_b, (1, enc_b.shape[0]))
-                    # ecn_f -> 順方向エンコーダLSTMブロックの1単語の出力
-                    # ecn_b -> 逆方向エンコーダLSTMブロックの1単語の出力
-                    alpi = score / s
-                    ct_f += alpi * enc_f.data[0]
-                    ct_b += alpi * enc_b.data[0]
-                # ctから出力単語を作る
-                ct_f = chainer.Variable(ct_f)
-                ct_b = chainer.Variable(ct_b)
-                target.append(
-                    F.tanh(self.Wc1(ct_f) + self.Wc2(ct_b) + self.Wc3(y_embeded)))
-            ys.append(F.concat(target, axis=0))
-        return ys
-
-    # }}}
+    def scoreDot(self, atts, ys):
+        xs = [self.W3(att) for att in atts]
+        dot = F.batch_matmul(ys,F.transpose(xs,(0,2,1)))
+        aws = F.softmax(dot,2)
+        cts = None
+        for x,aw in zip(xs,aws): # split batch
+            aw = F.expand_dims(aw,1)
+            x = F.tile(F.expand_dims(x,0),(aw.shape[0],1,1))
+            ct = F.batch_matmul(aw,x)
+            cts = ct if cts is None else F.concat((cts,ct),1)
+        cts = F.transpose(cts,(1,0,2))
+        ds = [F.tanh(self.Wc1(ct) + self.Wc2(y)) for y, ct in zip(ys,cts)] 
+        #for y,ct in zip(ys,cts):
+        #    d = F.tanh(self.Wc1(ct) + self.Wc2(y))
+        #    d = F.expand_dims(d,0)
+        #    ds = d if ds is None else F.concat((ds,d),0)
+        return ds
 
     def __call__(self, xs, ts=None, hx=None, cx=None, max_size=30):
         # エンコーダ側の学習
@@ -177,6 +110,7 @@ class GAtt(chainer.Chain):
                 ys_embeded = [self.embedy(y) for y in ys]
                 hx, cx, ys_embeded = self.decoder(hx, cx, ys_embeded)
                 ys_embeded = self.scoreDot(attentions, ys_embeded)
+                sys.exit()
                 ys = [xp.reshape(xp.argmax(F.softmax(self.W(y_embeded)).data), (1))
                       for y_embeded in ys_embeded]
                 ys_list.append(ys)
@@ -192,7 +126,6 @@ class GAtt(chainer.Chain):
             _, _, ys_embeded = self.decoder(hx, cx, ts_embeded)
             # attを計算する
             ys_embeded = self.scoreDot(attentions, ys_embeded)
-            # ts = self.scoreGeneral(attentions, ys_embeded)
             ys = [self.W(y) for y in ys_embeded]
             return ys
 
